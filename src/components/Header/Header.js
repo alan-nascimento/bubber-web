e660261e4499807432d30707887934853e7d7223
Alan Nascimento <alan.silva.n@hotmail.com>
2019-11-30 15:44:08 -0300

    Adding username to profile

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { Logo } from '~/assets';
 
 import { Container, Content, Profile } from './Header.styles';
 
 export default function Header() {
+  const profile = useSelector(state => state.user.profile);
+
   return (
     <Container>
       <Content>
@@ -16,12 +19,12 @@ export default function Header() {
         <aside>
           <Profile>
             <div>
-              <strong>Alan Nascimento</strong>
+              <strong>{profile.name}</strong>
               <Link to="/profile">Meu perfil</Link>
             </div>
             <img
               src="https://api.adorable.io/avatars/50/abott@adorable.png"
-              alt="Alan"
+              alt={profile.name}
             />
           </Profile>
         </aside>
--- src/components/Modal/Excursion/Excursion.js
+++ src/components/Modal/Excursion/Excursion.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 
 import { Form, Input, DatePicker, Select, InputNumber, Switch } from 'antd';
+import { Formik } from 'formik';
 
 import { Button } from '~/components';
 
@@ -47,115 +48,138 @@ export function Excursion({ form }) {
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <Form {...formItemLayout}>
-          <Form.Item
-            validateStatus="error"
-            help="O nome da excursão é obrigatório"
-          >
-            <Input placeholder="Nome da viagem" id="error" />
-          </Form.Item>
-          <Form.Item
-            validateStatus="error"
-            help="O endereço do local é obrigatório"
-          >
-            <Input placeholder="Endereço do local" id="warning" />
-          </Form.Item>
+        <Formik
+          initialValues={{}}
+          validate={values => {
+            const errors = {};
+            if (!values.email) {
+              errors.email = '*campo obrigatório';
+            } else if (
+              !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+            ) {
+              errors.email = 'E-mail inválido';
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              // dispatch(signInRequest(values.email, values.password));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({ handleSubmit, isSubmitting }) => (
+            <Form {...formItemLayout} onSubmit={handleSubmit}>
+              <Form.Item
+                validateStatus="error"
+                help="O nome da excursão é obrigatório"
+              >
+                <Input placeholder="Nome da viagem" id="error" />
+              </Form.Item>
+              <Form.Item
+                validateStatus="error"
+                help="O endereço do local é obrigatório"
+              >
+                <Input placeholder="Endereço do local" id="warning" />
+              </Form.Item>
 
-          <Form.Item style={{ marginBottom: 0, minWidth: '100%' }}>
-            <Form.Item
-              validateStatus="error"
-              help="Por favor, selecione uma data válida"
-            >
-              <div>
-                <RangePicker
-                  showTime={{ format: 'HH:mm' }}
-                  format="DD-MM-YYYY HH:mm"
-                  placeholder={['Data de partida', 'Data de volta']}
-                  onChange={onChange}
-                  onOk={onOk}
-                />
-              </div>
-            </Form.Item>
-          </Form.Item>
+              <Form.Item style={{ marginBottom: 0, minWidth: '100%' }}>
+                <Form.Item
+                  validateStatus="error"
+                  help="Por favor, selecione uma data válida"
+                >
+                  <div>
+                    <RangePicker
+                      showTime={{ format: 'HH:mm' }}
+                      format="DD-MM-YYYY HH:mm"
+                      placeholder={['Data de partida', 'Data de volta']}
+                      onChange={onChange}
+                      onOk={onOk}
+                    />
+                  </div>
+                </Form.Item>
+              </Form.Item>
 
-          <Form.Item
-            hasFeedback
-            validateStatus="error"
-            help="O tipo de excursão é obrigatório"
-          >
-            <Select defaultValue="Selecione o tipo de excursão">
-              <Option value="1">Praia</Option>
-              <Option value="2">Show</Option>
-              <Option value="3">Jogo do Corinthians</Option>
-            </Select>
-          </Form.Item>
+              <Form.Item
+                hasFeedback
+                validateStatus="error"
+                help="O tipo de excursão é obrigatório"
+              >
+                <Select defaultValue="Selecione o tipo de excursão">
+                  <Option value="1">Praia</Option>
+                  <Option value="2">Show</Option>
+                  <Option value="3">Jogo do Corinthians</Option>
+                </Select>
+              </Form.Item>
 
-          <section>
-            <Form.Item hasFeedback validateStatus="success">
-              <InputNumber min={5} max={100} defaultValue={5} />
-            </Form.Item>
+              <section>
+                <Form.Item hasFeedback validateStatus="success">
+                  <InputNumber min={5} max={100} defaultValue={5} />
+                </Form.Item>
 
-            <Form.Item hasFeedback validateStatus="success">
-              <InputNumber
-                defaultValue={0}
-                formatter={value =>
-                  `R$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-                }
-                parser={value => value.replace(/\$\s?|(,*)/g, '')}
-                onChange={onChange}
-              />
-            </Form.Item>
+                <Form.Item hasFeedback validateStatus="success">
+                  <InputNumber
+                    defaultValue={0}
+                    formatter={value =>
+                      `R$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                    }
+                    parser={value => value.replace(/\$\s?|(,*)/g, '')}
+                    onChange={onChange}
+                  />
+                </Form.Item>
 
-            <Form.Item hasFeedback validateStatus="success">
-              <InputNumber
-                defaultValue={0}
-                formatter={value =>
-                  `R$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-                }
-                parser={value => value.replace(/\$\s?|(,*)/g, '')}
-                onChange={onChange}
-              />
-            </Form.Item>
+                <Form.Item hasFeedback validateStatus="success">
+                  <InputNumber
+                    defaultValue={0}
+                    formatter={value =>
+                      `R$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                    }
+                    parser={value => value.replace(/\$\s?|(,*)/g, '')}
+                    onChange={onChange}
+                  />
+                </Form.Item>
 
-            <Form.Item hasFeedback validateStatus="success">
-              <InputNumber
-                defaultValue={0}
-                formatter={value =>
-                  `R$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-                }
-                parser={value => value.replace(/\$\s?|(,*)/g, '')}
-                onChange={onChange}
-              />
-            </Form.Item>
-          </section>
+                <Form.Item hasFeedback validateStatus="success">
+                  <InputNumber
+                    defaultValue={0}
+                    formatter={value =>
+                      `R$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+                    }
+                    parser={value => value.replace(/\$\s?|(,*)/g, '')}
+                    onChange={onChange}
+                  />
+                </Form.Item>
+              </section>
 
-          <Form.Item>
-            {getFieldDecorator('select', {
-              rules: [
-                {
-                  required: true,
-                  message:
-                    'Por favor, selecione pelo menos uma forma de pagamento',
-                  type: 'array',
-                },
-              ],
-            })(
-              <Select mode="multiple" placeholder="Formas de pagamento">
-                <Option value="credit-card">Cartão de crédito</Option>
-                <Option value="invoice">Boleto</Option>
-                <Option value="cash">Dinheiro</Option>
-              </Select>
-            )}
-          </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('select', {
+                  rules: [
+                    {
+                      required: true,
+                      message:
+                        'Por favor, selecione pelo menos uma forma de pagamento',
+                      type: 'array',
+                    },
+                  ],
+                })(
+                  <Select mode="multiple" placeholder="Formas de pagamento">
+                    <Option value="credit-card">Cartão de crédito</Option>
+                    <Option value="invoice">Boleto</Option>
+                    <Option value="cash">Dinheiro</Option>
+                  </Select>
+                )}
+              </Form.Item>
 
-          <section>
-            <Form.Item label="Lista de espera:">
-              {getFieldDecorator('switch', { valuePropName: 'checked' })(
-                <Switch />
-              )}
-            </Form.Item>
-          </section>
-        </Form>
+              <section>
+                <Form.Item label="Lista de espera:">
+                  {getFieldDecorator('switch', { valuePropName: 'checked' })(
+                    <Switch />
+                  )}
+                </Form.Item>
+              </section>
+            </Form>
+          )}
+        </Formik>
       </Modal>
     </div>
   );
